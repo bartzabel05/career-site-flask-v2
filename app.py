1934642506c0e4f69efc4e14b49b78bf1ff39924
bartzabel05 <72576658+bartzabel05@users.noreply.github.com>
2023-07-28 14:31:17 +0000

    Added Routes and Endpoints for dynamic data.

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 
 app=Flask(__name__)
 
+JOBS=[
+  {
+    "id":1,
+    "title":"Data Analyst",
+    "location":"Bengaluru,India",
+    "salary":'Rs:10,00,000'
+  },
+  {
+    "id":2,
+    "title":"Frontend Engineer",
+    "location":"Bengaluru,India",
+    "salary":'Rs:10,00,000'
+  },
+  {
+    "id":3,
+    "title":"Backend Engineer",
+    "location":"Bengaluru,India",
+    "salary":'Rs:10,00,000'
+  },
+]
+
 @app.route("/")
-def helloWorld():
-  return "Hello World"
-
-
-
+def helloWorld():  
+  return render_template('home.html',jobs=JOBS)
+  
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__=="__main__":
